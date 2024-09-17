ee18e354a890488ab9c82130cb30b52e426716df
Diogo Mello <diogoeng19@gmail.com>
2024-09-17 19:41:35 -0300

    :construction: Desenvolvendo Forms para entrada de saldos da conta

--- frontend/src/pages/Overheads.tsx
+++ frontend/src/pages/Overheads.tsx
@@ -4,7 +4,7 @@ import Expenses from "../components/Expenses";
 function Overheads(): JSX.Element {
     return (
         <article className="bg-[#19191a] w-11/12 m-auto my-5 p-3 text-white rounded-md">
-            <h1 className="bg-[#020202] w-60 m-auto mb-2 p-1 text-center text-2xl font-bold border-[1px] border-[#c9b977] rounded-md">Despesas</h1>
+            <h1 className="bg-[#020202] w-60 m-auto mb-4 p-1 text-center text-2xl font-bold border-b-4 border-r-4 border-[#c9b977] rounded-md">Despesas</h1>
             <div className="flex gap-4">
                 <div className="bg-[#020202] w-2/4 p-2 rounded-md group">
                     <h2 className="w-52 text-xl mb-3 border-b-2 border-white duration-75 group-hover:w-full group-hover:border-[#ecdda2] group-hover:text-center group-hover:text-[#ecdda2] group-hover:font-bold">Realizadas no Débito</h2>
