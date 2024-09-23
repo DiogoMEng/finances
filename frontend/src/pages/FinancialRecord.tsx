236fb0ba8252d9ebf6fb5d5943d1f5504e33556e
DIOGO DIAS MELLO <02843957222@trt8.jus.br>
2024-09-23 12:54:35 -0300

    :lipstick: feat: Estilizando Formulário

--- frontend/src/pages/FinancialRecord.tsx
+++ frontend/src/pages/FinancialRecord.tsx
@@ -4,33 +4,38 @@ export default function FinancialRecord(): JSX.Element {
             <h1 className="bg-[#020202] w-60 m-auto mb-2 p-1 text-center text-2xl font-bold border-b-4 border-r-4 border-[#c9b977] rounded-md">Novos Registros</h1>
             <form className="bg-[#020202] mx-auto w-2/6 p-3 rounded-md" action="">
                 <div className="mt-2">
-                    <span className="border-b-2 border-[#c9b977]">Inserir Novo Saldo</span>
-                    <div className="mt-5 items-center">
-                        <input id="balance" className="text-black p-1 mr-5 rounded-md placeholder:text-gray-400" type="text" placeholder="Novo Saldo (Ex: R$1200)" autoFocus/>
+                    <span className="border-b-2 border-[#c9b977]">Novo Saldo da Conta</span>
+                    <div className="relative mt-8 items-center group">
+                        <label className="absolute px-1 py-1 text-black pointer-events-none duration-300 group-has-[:focus]:flex group-has-[:focus]:text-white group-has-[:focus]:text-sm group-has-[:focus]:translate-y-[-28px]">Inserir Saldo (Ex: 1200)</label>
+                        <input id="balance" className="text-black p-1 mr-5 rounded-md" type="text"/>
                         <label htmlFor="paycheck">O pagamento já relizado?</label>
                         <input id="paycheck" className="w-4 h-4 ml-3" type="checkbox" defaultChecked/> 
                     </div>
                 </div>
                 <div className="mt-10">
                     <span className="border-b-2 border-[#c9b977]">Despesas em Débito/Dinheiro</span>
-                    <div className="mt-5 items-center">
-                        <input id="balance" className="text-black p-1 mr-5 rounded-md w-4/6 placeholder:text-gray-400" type="text" placeholder="Qual foi a despesa? (Ex: Amazon Store)"/>
+                    <div className="mt-8 items-center group">
+                        <label className="absolute px-1 py-1 text-black pointer-events-none duration-300 group-has-[:focus]:flex group-has-[:focus]:text-white group-has-[:focus]:text-sm group-has-[:focus]:translate-y-[-28px]">Qual foi a despesa? (Ex: Amazon Store)</label>
+                        <input id="balance" className="text-black p-1 mr-5 rounded-md w-4/6 placeholder:text-gray-400" type="text"/>
                     </div>
-                    <div className="mt-5 items-center">
-                        <input id="balance" className="text-black p-1 mr-5 rounded-md placeholder:text-gray-400" type="text" placeholder="Novo Saldo (Ex: R$100)"/>
-                        <label htmlFor="paycheck">O pagamento já relizado?</label>
-                        <input id="paycheck" className="w-4 h-4 ml-3" type="checkbox" defaultChecked/> 
+                    <div className="mt-7 items-center group">
+                        <label className="absolute px-1 py-1 text-black pointer-events-none duration-300 group-has-[:focus]:flex group-has-[:focus]:text-white group-has-[:focus]:text-sm group-has-[:focus]:translate-y-[-28px]">Novo Saldo (Ex: R$100)</label>
+                        <input id="balance" className="text-black p-1 mr-5 rounded-md placeholder:text-gray-400" type="text"/>
+                        <label>O pagamento já relizado?</label>
+                        <input className="w-4 h-4 ml-3" type="checkbox" defaultChecked/> 
                     </div>
                 </div>
                 <div className="mt-10">
                     <span className="border-b-2 border-[#c9b977]">Despesas em Crédito</span>
-                    <div className="mt-5 items-center">
-                        <input id="balance" className="text-black p-1 mr-5 rounded-md w-4/6 placeholder:text-gray-400" type="text" placeholder="Qual foi a despesa no crédito? (Ex: Lider)"/>
+                    <div className="mt-8 items-center group">
+                        <label className="absolute px-1 py-1 text-black pointer-events-none duration-300 group-has-[:focus]:flex group-has-[:focus]:text-white group-has-[:focus]:text-sm group-has-[:focus]:translate-y-[-28px]">Qual foi a despesa no crédito? (Ex: Lider)</label>
+                        <input id="balance" className="text-black p-1 mr-5 rounded-md w-4/6 placeholder:text-gray-400" type="text"/>
                     </div>
-                    <div className="mt-5 items-center">
+                    <div className="mt-7 items-center group">
+                        <label className="absolute px-1 py-1 text-black pointer-events-none duration-300 group-has-[:focus]:flex group-has-[:focus]:text-white group-has-[:focus]:text-sm group-has-[:focus]:translate-y-[-28px]">Novo Saldo (Ex: R$300)</label>
                         <input id="balance" className="text-black p-1 mr-5 rounded-md placeholder:text-gray-400" type="text" placeholder="Novo Saldo (Ex: R$300)"/>
-                        <label htmlFor="paycheck">O pagamento já relizado?</label>
-                        <input id="paycheck" className="w-4 h-4 ml-3" type="checkbox" defaultChecked/> 
+                        <label>O pagamento já relizado?</label>
+                        <input className="w-4 h-4 ml-3" type="checkbox" defaultChecked/> 
                     </div>
                 </div>
                 <div className="mt-10 flex justify-center gap-10">
